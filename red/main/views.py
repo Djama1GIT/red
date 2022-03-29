44e72e6835211d4795e509a7bd7b0b5edb08f9b8
Dj.ama1 <dj.ama1@mail.ru>
2022-03-30 00:23:54 +0300

    commit

--- red/main/views.py
+++ red/main/views.py
@@ -7,7 +7,7 @@ phone_number_ed = "+7(999)123-45-67"
 
 def MainView(request):
     promos = Promo.objects.filter(access="all")
-    promos_width = str(100 / (promos.count()+1))
+    promos_width = str(100 / (promos.count()+1))[:9]
     return render(request, 'red/index.html', {'title': 'RED | Home Page', 'phone': phone_number,
                                               'phone_ed': phone_number_ed, 'promos': promos,
                                               'promos_width': promos_width})
