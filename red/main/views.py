6212f5fdc193b6539592a777b2e7ea5407ec7511
Djama1GIT <dj.ama1@mail.ru>
2022-06-21 00:42:05 +0300

    shop, product-details and others edited

--- red/main/views.py
+++ red/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from .models import *
-from itertools import chain
+import json
 
 phone_number = "+79991234567"
 phone_number_ed = "+7(999)123-45-67"
@@ -27,6 +27,9 @@ def MainView(request):
     reviews = Review.objects.all()
     promos_width = str(100 / (promos.count() + 1))
     products = Product.objects.raw('SELECT * FROM main_product LIMIT 6')
+    for prod in products:
+        for k, v in json.loads(prod.image).items():
+            prod.image = k + "/" + v[0]
     return render(request, 'red/index.html',
                   {'title': 'RED | Home Page', 'phone': phone_number, 'phone_ed': phone_number_ed, 'promos': promos,
                    'promos_width': promos_width, 'fashions': fashions, 'fashion_minis': fashion_minis,
@@ -36,18 +39,41 @@ def MainView(request):
 
 
 def CartView(request):
-    return render(request, 'red/cart.html', {'title': 'RED | Cart', 'phone': phone_number,
-                                             'phone_ed': phone_number_ed, 'categories': categories})
+    return render(request, 'red/cart.html',
+                  {'title': 'RED | Cart', 'phone': phone_number,
+                   'phone_ed': phone_number_ed, 'categories': categories})
 
 
 def ProdDetailsView(request):
-    return render(request, 'red/product-details.html', {'title': 'RED | Product Details', 'phone': phone_number,
-                                                        'phone_ed': phone_number_ed, 'categories': categories})
+    if 'id' in request.GET:
+        product = Product.objects.raw('SELECT * FROM main_product WHERE id = ' + str(request.GET['id']))[0]
+        product.sizes = json.loads(product.sizes)
+        print(product.sizes)
+        product.count = 0
+        try:
+            for k, v in product.sizes.items():
+                product.count += v
+            product.image = json.loads(product.image)
+            # BETTER USE JSON!!!!!!!!!!!!!!!!!!!!!!!
+            product.related = Product.objects.raw(
+                'SELECT * FROM main_product WHERE (type = "' + product.type +
+                '" and subtype = "' + product.subtype + '" and id != ' + str(product.id) + ') LIMIT 5')
+            for prod in product.related:
+                for k, v in json.loads(prod.image).items():
+                    prod.image = k + "/" + v[0]
+        except Exception as exc:
+            return render(request, 'red/500.html')
+    else:
+        return render(request, 'red/404.html')
+    return render(request, 'red/product-details.html',
+                  {'title': 'RED | Product Details', 'phone': phone_number, 'product': product,
+                   'phone_ed': phone_number_ed, 'categories': categories, 'STATIC_URL': settings.STATIC_URL})
 
 
 def CheckoutView(request):
-    return render(request, 'red/checkout.html', {'title': 'RED | Checkout Page', 'phone': phone_number,
-                                                 'phone_ed': phone_number_ed, 'categories': categories})
+    return render(request, 'red/checkout.html',
+                  {'title': 'RED | Checkout Page', 'phone': phone_number, 'phone_ed': phone_number_ed,
+                   'categories': categories})
 
 
 def ShopView(request):
@@ -55,8 +81,19 @@ def ShopView(request):
     if len(types) == 2:
         types += [None, None]
     elif len(types) == 3:
-        types.append(None)
-    return render(request, 'red/shop.html', {'title': 'RED | Shop', 'phone': phone_number,
-                                             'phone_ed': phone_number_ed,
-                                             'type': types[2],
-                                             'subtype': types[3], 'categories': categories})
+        types += [None]
+    colors = {}
+    colors_tmp = Product.objects.raw(
+        "SELECT distinct color, count(color) as count, id FROM main_product group by color")  # не работает distinct, возможно из-за lite sql
+    for i in colors_tmp:
+        colors[i.color] = i.count
+    sizes = ["XS", "S", "M", "L", "XL", "XXL"]
+    max_price = Product.objects.raw("SELECT max(price) as max, id from main_product")[0].max
+    return render(request, 'red/shop.html',
+                  {'title': 'RED | Shop', 'phone': phone_number, 'phone_ed': phone_number_ed, 'type': types[2],
+                   'subtype': types[3], 'categories': categories, 'max_price': max_price,
+                   'colors': colors, "sizes": sizes})
+
+
+def err404(request, exception):
+    return render(request, 'red/404.html', status=404)
--- red/red/urls.py
+++ red/red/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import handler404, handler500
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,3 +21,5 @@ urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
+
+handler404 = 'main.views.err404'
