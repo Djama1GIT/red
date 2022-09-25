36804cf24e15d3dc217983c4e4bfc90b4088ed44
Djama1GIT <dj.ama1@mail.ru>
2022-09-25 11:59:55 +0300

    Now the rating is displayed(Product Details Page)

--- red/main/templatetags/repl.py
+++ red/main/templatetags/repl.py
@@ -6,3 +6,17 @@ register = template.Library()
 @register.filter
 def replace(value):
     return value.replace("-", " ")
+
+
+@register.filter
+def _rating(value):
+    value = int(value)
+    if value not in [1, 2, 3, 4, 5]:
+        value = 0
+    x = []
+    for i in range(value):
+        x += ['fa fa-star']
+    for i in range(5 - value):
+        x += ['fa fa-star-o']
+    print(x)
+    return x
